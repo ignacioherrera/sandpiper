587f35a9a27f441123b5699bffcb385ba4225faf
ignacio <iherreraaguero5@gmail.com>
2019-09-16 09:55:21 -0400

    boton header an other fixes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,71 +1,71 @@
 // Custom Scripts for Array Template //
 
-jQuery(function($) {
-    "use strict";
+jQuery(function ($) {
+  "use strict";
 
-        // get the value of the bottom of the #main element by adding the offset of that element plus its height, set it as a variable
-        var mainbottom = $('#main').offset().top;
+  // get the value of the bottom of the #main element by adding the offset of that element plus its height, set it as a variable
+  var mainbottom = $('#main').offset().top;
 
-        // on scroll,
-        $(window).on('scroll',function(){
+  // on scroll,
+  $(window).on('scroll', function () {
 
-        // we round here to reduce a little workload
-        stop = Math.round($(window).scrollTop());
-        if (stop > mainbottom) {
-            $('.navbar').addClass('past-main');
-            $('.navbar').addClass('effect-main')
-        } else {
-            $('.navbar').removeClass('past-main');
-       }
+    // we round here to reduce a little workload
+    stop = Math.round($(window).scrollTop());
+    if (stop > mainbottom) {
+      $('.navbar').addClass('past-main');
+      $('.navbar').addClass('effect-main')
+    } else {
+      $('.navbar').removeClass('past-main');
+    }
 
-      });
+  });
 
 
   // Collapse navbar on click
 
-   $(document).on('click.nav','.navbar-collapse.in',function(e) {
-    if( $(e.target).is('a') ) {
-    $(this).removeClass('in').addClass('collapse');
-   }
+  $(document).on('click.nav', '.navbar-collapse.in', function (e) {
+    if ($(e.target).is('a')) {
+      $(this).removeClass('in').addClass('collapse');
+    }
   });
 
 
-    /*-----------------------------------
-    ----------- Scroll To Top -----------
-    ------------------------------------*/
+  /*-----------------------------------
+  ----------- Scroll To Top -----------
+  ------------------------------------*/
 
-    $(window).on('scroll', function () {
-      if ($(this).scrollTop() > 1000) {
-          $('#back-top').fadeIn();
-      } else {
-          $('#back-top').fadeOut();
-      }
-    });
-    // scroll body to 0px on click
-    $('#back-top').on('click', function () {
-      $('#back-top').tooltip('hide');
-      $('body,html').animate({
-          scrollTop: 0
-      }, 1500);
-      return false;
-    });
+  $(window).on('scroll', function () {
+    if ($(this).scrollTop() > 1000) {
+      $('#back-top').fadeIn();
+    } else {
+      $('#back-top').fadeOut();
+    }
+  });
+  // scroll body to 0px on click
+  $('#back-top').on('click', function () {
+    $('#back-top').tooltip('hide');
+    $('body,html').animate({
+      scrollTop: 0
+    }, 1500);
+    return false;
+  });
 
 
-    /*-------- Owl Carousel ---------- */
+  /*-------- Owl Carousel ---------- */
 
-      $(".review-cards").owlCarousel({
-        slideSpeed: 200,
-        items: 1,
-        singleItem: true,
-        autoplay:true,
-        autoplayTimeout:2000,
-        autoplayHoverPause:true,
-        pagination: false,
-      });
+  $(".review-cards").owlCarousel({
+    slideSpeed: 200,
+    items: 1,
+    singleItem: true,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    pagination: false,
+  });
 
 
   /* ------ jQuery for Easing min -- */
-  (function($) {
+  (function ($) {
     "use strict"; // Start of use strict
 
     // Smooth scrolling using jQuery easing
@@ -83,7 +83,7 @@ jQuery(function($) {
     });
 
     // Closes responsive menu when a scroll trigger link is clicked
-    $('.js-scroll-trigger').on('click', function() {
+    $('.js-scroll-trigger').on('click', function () {
       $('.navbar-collapse').collapse('hide');
     });
 
@@ -96,54 +96,62 @@ jQuery(function($) {
   })(jQuery); // End of use strict
 
 
-/* --------- Wow Init ------ */
+  /* --------- Wow Init ------ */
 
   new WOW().init();
 
 
   /* ----- Counter Up ----- */
 
-$('.counter').counterUp({
-		delay: 10,
-		time: 1000
-});
+  $('.counter').counterUp({
+    delay: 10,
+    time: 1000
+  });
 
-/*----- Preloader ----- */
+  /*----- Preloader ----- */
 
-    $(window).on('load', function() {
-  		setTimeout(function() {
-        $('#loading').fadeOut('slow', function() {
-        });
-      }, 3000);
-    });
+  $(window).on('load', function () {
+    setTimeout(function () {
+      $('#loading').fadeOut('slow', function () {
+      });
+    }, 3000);
+  });
 
 
-/*----- Subscription Form ----- */
+  /*----- Subscription Form ----- */
 
-$(document).ready(function() {
-      var  clientHeight = $(window).height();
-      var width = $(window).width();
+  $(document).ready(function () {
+    var clientHeight = $(window).height();
+    var width = $(window).width();
     //Header al 100% de la pantalla del cliente
-    if(width>768){
+    if (width > 768) {
       $('.fullHeightHeader').height(clientHeight);
     }
-    
-     // jQuery Validation
-     $("#chimp-form").validate({
-         // if valid, post data via AJAX
-         submitHandler: function(form) {
-             $.post("assets/php/subscribe.php", { email: $("#chimp-email").val() }, function(data) {
-                 $('#response').html(data);
-             });
-         },
-         // all fields are required
-         rules: {
-             email: {
-                 required: true,
-                 email: true
-             }
-         }
-     });
- });
+    $(window).resize(function () {
+      clientHeight = $(window).height();
+      width = $(window).width();
+      //Header al 100% de la pantalla del cliente
+      if (width > 768) {
+        $('.fullHeightHeader').height(clientHeight);
+      }
+    });
+
+    // jQuery Validation
+    $("#chimp-form").validate({
+      // if valid, post data via AJAX
+      submitHandler: function (form) {
+        $.post("assets/php/subscribe.php", { email: $("#chimp-email").val() }, function (data) {
+          $('#response').html(data);
+        });
+      },
+      // all fields are required
+      rules: {
+        email: {
+          required: true,
+          email: true
+        }
+      }
+    });
+  });
 
 });
